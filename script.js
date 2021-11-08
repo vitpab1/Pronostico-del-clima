c2a784e79be62e0ccdd2ade58de974f9f74ec2c8
Pablo Vittar <vitpablo1@gmail.com>
2021-11-08 17:54:46 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,122 @@
+
+
+let weather = {
+    apiKey: "c3bd835d4e2d28d352a45c1773483f83",
+    fetchWeather: function (city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey + "&lang=sp"
+      )
+        .then((response) => {
+          if (!response.ok) {
+            alert("No se encontro la ciudad :(");
+            throw new Error("No se encontro la ciudad ");
+          }
+          return response.json();
+        })
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function (data) {
+      const { name } = data;
+      const { icon, description } = data.weather[0];
+      const { temp, humidity } = data.main;
+      const {feels_like} = data.main; 
+      
+      document.querySelector(".city").innerText =  name;
+      document.querySelector(".icon").src =
+        "https://openweathermap.org/img/wn/" + icon + ".png";
+      document.querySelector(".description").innerText = description;
+      document.querySelector(".temp").innerText = Math.round(temp) + "°C";
+      document.querySelector(".humidity").innerText =
+        "Humedad: " + humidity + "%";
+      document.querySelector(".feels_like").innerText =
+        "Sensacion Termica: " + Math.round( feels_like) + "°C";
+      document.querySelector(".weather").classList.remove("loading");
+      document.body.style.backgroundImage =
+        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+    },
+    search: function () {
+      this.fetchWeather(document.querySelector(".search-bar").value);
+    },
+  };
+
+
+  document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+  });
+
+
+  
+let geocode = {
+  reverseGeocode: function (latitude, longitude) {
+    var apikey = "c7970576c3b847318dd5a0fbbb48f281";
+
+    var api_url = "https://api.opencagedata.com/geocode/v1/json";
+
+    var request_url =
+      api_url +
+      "?" +
+      "key=" +
+      apikey +
+      "&q=" +
+      encodeURIComponent(latitude + "," + longitude) +
+      "&pretty=1" +
+      "&no_annotations=1";
+
+    // see full list of required and optional parameters:
+    // https://opencagedata.com/api#forward
+
+    var request = new XMLHttpRequest();
+    request.open("GET", request_url, true);
+
+    request.onload = function () {
+      // see full list of possible response codes:
+      // https://opencagedata.com/api#codes
+
+      if (request.status == 200) {
+        // Success!
+        var data = JSON.parse(request.responseText);
+        weather.fetchWeather(data.results[0].components.city);
+        console.log(data.results[0].components.city)
+      } else if (request.status <= 500) {
+        // We reached our target server, but it returned an error
+
+        console.log("unable to geocode! Response code: " + request.status);
+        var data = JSON.parse(request.responseText);
+        console.log("error msg: " + data.status.message);
+      } else {
+        console.log("server error");
+      }
+    };
+
+    request.onerror = function () {
+      // There was a connection error of some sort
+      console.log("unable to connect to server");
+    };
+
+    request.send(); // make the request
+  },
+  getLocation: function() {
+    function success (data) {
+      geocode.reverseGeocode(data.coords.latitude, data.coords.longitude);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, console.error);
+    }
+    else {
+      weather.fetchWeather("Denver");
+    }
+  }
+};
+  
+  document
+    .querySelector(".search-bar")
+    .addEventListener("keyup", function (event) {
+      if (event.key == "Enter") {
+        weather.search();
+      }
+    });
+
+    geocode.getLocation();
